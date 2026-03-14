5f687882f00f8bbedff31a6cbef31bca662c1a7d
Bhatt Akshat <akshatbhatt0786@gmail.com>
2026-03-14 09:10:59 +0530

    init: Django settings, INSTALLED_APPS, urls scaffold, apps package

--- apps/operations/apps.py
+++ apps/operations/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class OperationsConfig(AppConfig):
-    name = 'operations'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.operations'
